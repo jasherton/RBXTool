e7c27a095590dbdfeb4f1c0cf9270f53937ace00
Mike <jjelamb@gmail.com>
2023-09-13 20:13:21 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 import io
 import zipfile
 import requests
@@ -7,21 +8,22 @@ import json
 FileVersion = "1"
 GitRepo = "https://api.github.com/repos/jasherton/RBXTool/releases/latest"
 
-res = requests.get(GitRepo,headers={"User-Agent":"Python"})
-if res.status_code == 200:
-    content = json.loads(res.content.decode("utf-8"))
-    res = requests.get(content["assets"][0]["browser_download_url"])
-    data = zipfile.ZipFile(io.BytesIO(res.content))
-    names = data.namelist()
-    versionText = None
-    if "app.py" in names:
-        text = data.read("app.py").decode("utf-8")
-        text += "FileVersion = \"1000\""
-        match = text.find("FileVersion = \"")
-        if match != -1:
-            versionText = text[match+15:text.find("\"",match+16)]
-    if versionText and versionText != FileVersion:
-        data.extractall()
+if not Path(".git").exists():
+    res = requests.get(GitRepo,headers={"User-Agent":"Python"})
+    if res.status_code == 200:
+        content = json.loads(res.content.decode("utf-8"))
+        res = requests.get(content["assets"][0]["browser_download_url"])
+        data = zipfile.ZipFile(io.BytesIO(res.content))
+        names = data.namelist()
+        versionText = None
+        if "app.py" in names:
+            text = data.read("app.py").decode("utf-8")
+            text += "FileVersion = \"1000\""
+            match = text.find("FileVersion = \"")
+            if match != -1:
+                versionText = text[match+15:text.find("\"",match+16)]
+        if versionText and versionText != FileVersion:
+            data.extractall()
 
 from interface import Main
 Main()
