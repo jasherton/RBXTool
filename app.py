ecc16f030d1df0d8f47d88f23d855da5fff3baf2
Mike <jjelamb@gmail.com>
2023-09-14 19:20:46 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import requests
 import json
 
 #Automatic Updating
-FileVersion = "3"
+FileVersion = "4"
 GitRepo = "https://api.github.com/repos/jasherton/RBXTool/releases/latest"
 
 if not Path(".git").exists():
